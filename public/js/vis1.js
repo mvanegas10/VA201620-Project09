05c1a6c3979203af32f4f10412e7b1abdc31c73f
aiMojica10 <ai.mojica10@uniandes.edu.co>
2016-12-03 21:54:53 -0500

    scatterplot_withFake data

--- public/js/vis1.js
+++ public/js/vis1.js
@@ -89,6 +89,7 @@ socket.on(SHOW_TICKETS, function (data) {
     })
 
     stackedBarChart(dataFinal);
+    scatterplot();
 
 });
 
@@ -97,7 +98,7 @@ socket.on(SHOW_TICKETS, function (data) {
 // ------------------------------------------------------
 function lineChart(dataX, dataY, dataZ, daySelected) {
     var dataAVG = Array.apply(null, Array(dataY.length)).map(Number.prototype.valueOf,Number(avg));
-    dataAVG.unshift("promedio");     
+    dataAVG.unshift("promedio");
     var arr = dataY.slice(0);
     arr.splice(0,1);
     arr = arr.map(Number);
@@ -126,7 +127,7 @@ function lineChart(dataX, dataY, dataZ, daySelected) {
             selection: {
                 enabled: true,
                 multiple: true,
-                isselectable: function(d) {                    
+                isselectable: function(d) {
                     return !(d.id === "promedio");
                 }
             },
@@ -194,7 +195,7 @@ function lineChart(dataX, dataY, dataZ, daySelected) {
             interaction: {
                 enabled: false
             }
-        },        
+        },
         tooltip: {
             format: {
                 title: function (d) {
@@ -230,7 +231,7 @@ function lineChart(dataX, dataY, dataZ, daySelected) {
         },
         transition: {
             duration: null
-        },       
+        },
         subchart: {
             show: true,
             size: {
@@ -330,9 +331,42 @@ function stackedBarChart(columnsData) {
           .style('font-size', '16px');
     }
 }
-
+// visualizacion scatterplot
 function scatterplot(dataScatter) {
-    console.log(dataScatter);
+
+  chart = c3.generate({
+          size: {
+            height: 500,
+            width: 700
+        },
+        bindto: '#scatterplot',
+    data: {
+        xs: {
+            setosa: 'setosa_x',
+            versicolor: 'versicolor_x',
+        },
+        // iris data from R
+        columns: [
+            ["setosa_x", 3.5, 3.0, 3.2, 3.1, 3.6, 3.9, 3.4, 3.4, 2.9, 3.1, 3.7, 3.4, 3.0, 3.0, 4.0, 4.4, 3.9, 3.5, 3.8, 3.8, 3.4, 3.7, 3.6, 3.3, 3.4, 3.0, 3.4, 3.5, 3.4, 3.2, 3.1, 3.4, 4.1, 4.2, 3.1, 3.2, 3.5, 3.6, 3.0, 3.4, 3.5, 2.3, 3.2, 3.5, 3.8, 3.0, 3.8, 3.2, 3.7, 3.3],
+            ["versicolor_x", 3.2, 3.2, 3.1, 2.3, 2.8, 2.8, 3.3, 2.4, 2.9, 2.7, 2.0, 3.0, 2.2, 2.9, 2.9, 3.1, 3.0, 2.7, 2.2, 2.5, 3.2, 2.8, 2.5, 2.8, 2.9, 3.0, 2.8, 3.0, 2.9, 2.6, 2.4, 2.4, 2.7, 2.7, 3.0, 3.4, 3.1, 2.3, 3.0, 2.5, 2.6, 3.0, 2.6, 2.3, 2.7, 3.0, 2.9, 2.9, 2.5, 2.8],
+            ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+            ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+        ],
+        type: 'scatter'
+    },
+    axis: {
+        x: {
+            label: 'Sepal.Width',
+            tick: {
+                fit: false
+            }
+        },
+        y: {
+            label: 'Petal.Width'
+        }
+    }
+});
+    console.log("scatterplot AAAAAAAAAAAAAAAAAAAAAA");
 }
 
 // ADDITIONAL FUNCTIONS
